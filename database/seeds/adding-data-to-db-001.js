159ac3e77780983e589e013078da36250a630c80
Modurotolu Olokode <durolawk@gmail.com>
2019-11-16 13:12:51 +0100

    seeded user table, used bcrypt to hash password

--- database/seeds/adding-data-to-db-001.js
+++ database/seeds/adding-data-to-db-001.js
@@ -1,13 +1,14 @@
+const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {username: 'test1', password: bcrypt.hashSync('test1', 10)},
+        {username: 'test2', password: bcrypt.hashSync('test2', 10)},
+        {username: 'test3', password: bcrypt.hashSync('test3', 10)}
       ]);
     });
 };
