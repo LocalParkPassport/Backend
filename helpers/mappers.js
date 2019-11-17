cd0a37068044940741159431df75c04c1bc24f23
Modurotolu Olokode <durolawk@gmail.com>
2019-11-17 22:51:20 +0100

    created mapper function to deal with all integer to boolean in park object

--- helpers/mappers.js
+++ helpers/mappers.js
@@ -7,3 +7,14 @@ function integerToBoolean(int) {
     return int === 1 ? true : false;
 }
 
+function parkPropertyToBoolean(park) {
+    return {
+        ...park,
+        "dog park": integerToBoolean(park["dog park"]),
+        wildlife: integerToBoolean(park.wildlife),
+        "hiking trails": integerToBoolean(park["hiking trails"]),
+        "disc golf": integerToBoolean(park["disc golf"]),
+        "open spaces": integerToBoolean(park["open spaces"]),
+        "climbing trees": integerToBoolean(park["climbing trees"]),
+    }
+}
