895c65b861d71f45ca4f054ba9dbf092869b523b
Modurotolu Olokode <durolawk@gmail.com>
2019-11-18 14:26:41 +0100

    used %% with like to allow quering all on undefined

--- parks/parks-model.js
+++ parks/parks-model.js
@@ -30,19 +30,20 @@ async function add(park) {
 function findByPark(body) {
     search = mappers.parkPropertyToInteger(body);
 
-    const a = search["dog park"] !== undefined ? `%${search["dog park"]}%` : "";
-    const b = search["disc golf"] !== undefined ? `%${search["disc golf"]}%` : "";
-    console.log(a, b);
+    const a = search["dog park"] !== undefined ? `%${search["dog park"]}%` : 0 || 1;
+    const b = search["wildlife"] !== undefined ? `%${search["wildlife"]}%` : 0 || 1;
+    const c = search["hiking trails"] !== undefined ? `%${search["hiking trails"]}%` : 0 || 1;
+    console.log(a, b, c);
     
     return db('parks')
         .where('name', 'like', `%${search.name}%`)
         .where('location', 'like', `%${search.location}%`)
         .where('description', 'like', `%${search.description}%`)
-        .where('dog park', 'like', search["dog park"] !== undefined ? `%${search["dog park"]}%` : "")
-        .where('wildlife', 'like', search["wildlife"] !== undefined ? `%${search["wildlife"]}%` : "")
-        .where('hiking trails', 'like', search["hiking trails"] !== undefined ? `%${search["hiking trails"]}%` : "")
-        .where('disc golf', 'like', search["disc golf"] !== undefined ? `%${search["disc golf"]}%` : "")
-        .where('open spaces', 'like', search["open spaces"] !== undefined ? `%${search["open spaces"]}%` : "")
-        .where('climbing trees', 'like', search["climbing trees"] !== undefined ? `%${search["climbing trees"]}%` : "")
+        .where('dog park', 'like', search["dog park"] !== undefined ? `%${search["dog park"]}%` : "%%")
+        .where('wildlife', 'like', search["wildlife"] !== undefined ? `%${search["wildlife"]}%` : "%%")
+        .where('hiking trails', 'like', search["hiking trails"] !== undefined ? `%${search["hiking trails"]}%` : "%%")
+        .where('disc golf', 'like', search["disc golf"] !== undefined ? `%${search["disc golf"]}%` : "%%")
+        .where('open spaces', 'like', search["open spaces"] !== undefined ? `%${search["open spaces"]}%` : "%%")
+        .where('climbing trees', 'like', search["climbing trees"] !== undefined ? `%${search["climbing trees"]}%` : "%%")
         .then(parks => parks.map(park => mappers.parkPropertyToBoolean(park)));
 }
