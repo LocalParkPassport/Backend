7b3a31b00c36da841a2908ba45f4868f8fbf4d03
Modurotolu Olokode <durolawk@gmail.com>
2019-11-20 17:37:36 +0100

    tested for login and register

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbConfig')
+
+beforeAll(async () => {
+    await db('users').truncate();
+})
+
+const input = {
+    username: 'testing',
+    password: 'testing'
+}
+
+describe('users authorization', () => {
+    describe('POST /users signup', () => {
+        test('should return 201, with testing as user and correct content-type', () => {
+            return request(server)
+                .post('/api/auth/register')
+                .send(input)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(201)
+                .then(res => {
+                    (res.body.username, 'testing')
+                })
+        });
+
+        test('should return 200, with testing as user and correct content-type', (done) => {
+            request(server)
+                .post('/api/auth/login')
+                .send(input)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    done()
+                })
+        });
+    });
+
+})
